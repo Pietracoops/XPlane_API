9b17f57ba7c17455cb6294464f975e5f53e3bf40
pietracoops <pietracoops@gmail.com>
2022-09-15 20:01:11 -0400

    removing josh' scripts

--- _josh_src/API_logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import sys
-logger = logging.getLogger("XPlane_Logger")
-logging.basicConfig(filename='API_log.txt',
-                    filemode='a',
-                    level=logging.INFO,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
